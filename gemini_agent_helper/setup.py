01b8a4472f099a69b08d19f386f2d879b9b531e0
Huriya Syed <167598780+huriyasyed3@users.noreply.github.com>
2025-06-14 17:20:59 +0500

    update

--- gemini_agent_helper/setup.py
+++ gemini_agent_helper/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='gemini-helper',
-    version='0.1',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=[
         'python-dotenv',
